3da529aee006117400fe302b389c79f8e5caf926
Larnelle Letts <larnelleletts11@outlook.com>
2018-03-26 04:28:55 +0000

    your commit message

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -24,6 +24,52 @@ Vue.component('app-header', {
     data: function() {}
 });
 
+Vue.component('news-list', {
+    template: ` <div class='news'>
+                    <h2>News</h2>
+                    <div class="form-inline d-flex justify-content-center">
+    <div class="form-group mx-sm-3 mb-2">
+        <label class="sr-only" for="search">Search</label>
+        <input type="search" name="search" v-model="searchTerm"
+id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
+search term here" />
+        <p>You are searching for {{ searchTerm }}</p>
+    </div>
+</div>
+                    <div id='thing'></div>
+                    <div class='containers'>
+                    <div v-for='article in articles' class='news__item grip-item'>
+                    <h5>{{ article.title }}</h5>
+                    <img class='news' v-bind:src='article.urlToImage' height="300" width="300"/>
+                    <p>{{article.description}}</p>
+                    </div>
+                    
+                    
+                    </div> ` 
+    ,
+    created: function() {
+        let self = this;
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=cd48b317059c494f9454123c900ca9af')
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            console.log(data);
+            self.articles = data.articles;
+}); 
+        
+    },
+    data: function() {
+        return {
+            articles: [],
+            searchTerm: ''
+        }
+        
+    }
+    
+    
+});
+
 Vue.component('app-footer', {
     template: `
         <footer>
@@ -46,4 +92,5 @@ let app = new Vue({
         welcome: 'Hello World! Welcome to VueJS'
     }
 });
+
 
